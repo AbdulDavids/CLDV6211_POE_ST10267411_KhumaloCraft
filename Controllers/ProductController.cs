4e9ebbea8e553c44d06c686022ad56b268a6be99
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-04-01 13:41:22 +0200

    Initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CLDV6211_POE_ST10267411_KhumaloCraft.Controllers
+{
+    public class ProductsController : Controller
+    {
+       
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLDV6211_POE_ST10267411_KhumaloCraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
